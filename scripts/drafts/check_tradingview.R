888c4cf2507bad5f67033a3e2ab01a6a8d902544
Johan-rosa <j.rosa@bancentral.gov.do>
2026-08-27 17:20:14 -0400

    portfolio activity

--- scripts/drafts/check_tradingview.R
+++ scripts/drafts/check_tradingview.R
@@ -4,6 +4,13 @@ send <- readr::read_csv("data/tradingView.csv")
 good_symbols <- recv$Symbol |> stringr::str_remove(".+:") |>
   unique()
 
+readxl::read_excel("data/portfolio_activity.xlsx") |>
+  dplyr::filter(ticker %in% c("EQX", "ORCL", "NOW")) |>
+  setNames(names(send)) |>
+  readr::write_csv("data/moretransacionts.csv")
+
+send |>
+  dplyr::filter(Symbol == "EQX")
 send |>
   janitor::clean_names() |>
   dplyr::filter(!symbol %in% good_symbols) |>
